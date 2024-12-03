29b51199c000cfba7eecd563010ee153e58dddb2
Sourabh kumar <161230977+Sourabhkumarcodes@users.noreply.github.com>
2024-12-03 20:49:25 +0530

    Add files via upload

---
+++ SubString.java
@@ -0,0 +1,7 @@
+public class SubString {
+    public static void main(String args[]){
+        String str="hello world";
+        System.out.println(str.substring(3));
+        System.out.println(str.substring(3, 6));
+    }
+}
