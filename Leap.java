f6e1140a0f17f63784687c0def08af4ea1d2dbc4
Sourabh kumar <161230977+Sourabhkumarcodes@users.noreply.github.com>
2024-12-10 17:29:14 +0530

    Add files via upload

---
+++ Leap.java
@@ -0,0 +1,10 @@
+public class Leap {
+    public static void main(String args[]){
+        int year =2024;
+        if((year%4==0&&(year%100!=0))||(year%400==0)){
+            System.out.println("leap");
+        }else{
+            System.out.println("noot");
+        }
+    }
+}
